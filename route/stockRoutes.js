034d48910540c8715aae57da55a54d603ff06b67
bitcat <kurohaj96@gmail.com>
2023-09-10 19:44:56 +0900

    a

--- route/stockRoutes.js
+++ route/stockRoutes.js
@@ -1,18 +1,16 @@
-
-// routes/stockRoutes.js
 const express = require('express');
 const Stock = require('../models/Stock');
 
 const router = express.Router();
 
-let totalSales = 0; // 売上合計を保持
+let totalSales = 0;
 
-// (1) 在庫の更新、作成
+// (1) Create or update stock
 router.post('/stocks', async (req, res) => {
   const { name, amount } = req.body;
 
   if (!Number.isInteger(amount)) {
-      return res.status(400).json({ message: 'ERROR' });
+    return res.status(400).json({ message: 'ERROR' });
   }
   
   try {
@@ -28,12 +26,12 @@ router.post('/stocks', async (req, res) => {
     
     res.status(200).json({ name, amount: stockItem.amount });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-// (2) 在庫チェック
+// (2) Check stock
 router.get('/stocks', async (req, res) => {
   try {
     const stocks = await Stock.find();
@@ -45,112 +43,47 @@ router.get('/stocks', async (req, res) => {
     
     res.status(200).json(response);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-
-// (1) 在庫の更新、作成
-router.post('/stocks', async (req, res) => {
-  const { name, amount } = req.body;
-
-  if (!Number.isInteger(amount)) {
-      return res.status(400).json({ message: 'ERROR' });
-  }
-  
-  try {
-    let stockItem = await Stock.findOne({ name });
-    
-    if (stockItem) {
-      stockItem.amount += amount;
-      await stockItem.save();
-    } else {
-      stockItem = new Stock({ name, amount });
-      await stockItem.save();
-    }
-    
-    res.status(200).json({ name, amount: stockItem.amount });
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-// (2) 在庫チェック
-router.get('/stocks', async (req, res) => {
-  try {
-    const stocks = await Stock.find();
-    const response = {};
-    
-    stocks.forEach(stock => {
-      response[stock.name] = stock.amount;
-    });
-    
-    res.status(200).json(response);
-  } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-// (3) 販売
+// (3) Sales
 router.post('/sales', async (req, res) => {
-  const { name, amount } = req.body;  
-  let { price } = req.body;
+  const { name, amount = 0 } = req.body;  // Set default value for amount
+  const { price } = req.body;
 
-  // priceが指定されていない、またはnumberでない場合には何もしない
   if (price && typeof price !== 'number') {
     return res.status(400).json({ error: 'Price must be a number when provided.' });
   }
 
   try {
-      const stockItem = await Stock.findOne({ name });
-      
-      if (!stockItem || stockItem.amount < amount) {
-          return res.status(400).json({ error: 'Not enough stock' });
-      }
-      
-      stockItem.amount -= amount;
-      
-      // priceとamountが存在する場合のみtotalSalesに加算
-      if (price) {
-        totalSales += amount ? amount * price : price;
-      }
+    const stockItem = await Stock.findOne({ name });
+    
+    if (!stockItem || stockItem.amount < amount) {
+      return res.status(400).json({ error: 'Not enough stock' });
+    }
+    
+    stockItem.amount -= amount;
+    
+    if (price) {
+      totalSales += amount ? amount * price : price;
+    }
 
-      await stockItem.save();
-      
-      res.status(200).json({ name, amount });
-  } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-// (4) 売り上げチェック
-router.get('/sales', (req, res) => {
-  res.status(200).json({ sales: totalSales });
-});
-
-// (5) 全削除
-router.delete('/stocks', async (req, res) => {
-  try {
-    await Stock.deleteMany({});
-    totalSales = 0;
-    res.status(200).end();
+    await stockItem.save();
+    
+    res.status(200).json({ name, amount });
   } catch (error) {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-module.exports = router;
-
-
-// (4) 売り上げチェック
+// (4) Check sales
 router.get('/sales', (req, res) => {
   res.status(200).json({ sales: totalSales });
 });
 
-// (5) 全削除
+// (5) Delete all
 router.delete('/stocks', async (req, res) => {
   try {
     await Stock.deleteMany({});
