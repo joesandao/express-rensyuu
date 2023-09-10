18c017466bea0af04f24a5e26c84e944fa80f861
bitcat <kurohaj96@gmail.com>
2023-09-10 19:26:00 +0900

    a

--- route/stockRoutes.js
+++ route/stockRoutes.js
@@ -50,6 +50,50 @@ router.get('/stocks', async (req, res) => {
   }
 });
 
+
+// (1) 在庫の更新、作成
+router.post('/stocks', async (req, res) => {
+  const { name, amount } = req.body;
+
+  if (!Number.isInteger(amount)) {
+      return res.status(400).json({ message: 'ERROR' });
+  }
+  
+  try {
+    let stockItem = await Stock.findOne({ name });
+    
+    if (stockItem) {
+      stockItem.amount += amount;
+      await stockItem.save();
+    } else {
+      stockItem = new Stock({ name, amount });
+      await stockItem.save();
+    }
+    
+    res.status(200).json({ name, amount: stockItem.amount });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// (2) 在庫チェック
+router.get('/stocks', async (req, res) => {
+  try {
+    const stocks = await Stock.find();
+    const response = {};
+    
+    stocks.forEach(stock => {
+      response[stock.name] = stock.amount;
+    });
+    
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // (3) 販売
 router.post('/sales', async (req, res) => {
   const { name, amount, price = 1 } = req.body;  // priceのデフォルト値を1に設定
@@ -70,6 +114,25 @@ router.post('/sales', async (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+
+// (4) 売り上げチェック
+router.get('/sales', (req, res) => {
+  res.status(200).json({ sales: totalSales });
+});
+
+// (5) 全削除
+router.delete('/stocks', async (req, res) => {
+  try {
+    await Stock.deleteMany({});
+    totalSales = 0;
+    res.status(200).end();
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
 
 
 // (4) 売り上げチェック
