8198fc75f22ad9620e2763ffbca6b4d85dcd14d3
bitcat <kurohaj96@gmail.com>
2023-09-10 16:33:37 +0900

    a

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-// server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const stockRoutes = require('./route/stockRoutes');
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/aws', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Mongooseとの接続設定
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// 接続が成功した場合のリスナー
+mongoose.connection.once('open', () => {
+    console.log('Successfully connected to MongoDB.');
+});
+
+// エラーが発生した場合のリスナー
+mongoose.connection.on('error', err => {
+    console.error('Failed to connect to MongoDB:', err);
+});
 
 app.use(express.json());
 app.use('/v1', stockRoutes);
 
-mongoose.connection.on('error', err => {
-  console.error('Failed to connect to MongoDB', err);
-});
-
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
